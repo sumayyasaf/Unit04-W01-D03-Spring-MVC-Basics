1a197a1f3077cf068716297e8f6b1debae4b63df
Omar <omaraakamal@gmail.com>
2025-04-30 11:16:48 +0300

    update

---
+++ mvc-lesson/src/main/java/com/dci/mvc_lesson/MainController.java
@@ -0,0 +1,17 @@
+package com.dci.mvc_lesson;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model){
+        Student student1 = new Student(1,"Mihai","miahi@gmail.com",false);
+        model.addAttribute("student",student1);
+        return "home";
+    }
+}
